1b69b1ad39d54a8602d51fe2128009a7ee918136
chhaichivon <chivonchhai@hotmail.com>
2017-11-27 17:24:34 +0700

    init

---
+++ src/main/java/com/giant/egiant/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.giant.egiant;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EGiantApplication.class);
+	}
+
+}
---
+++ src/main/java/com/giant/egiant/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.giant.egiant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author by chhai.chivon  on 11/27/2017.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/" , method = RequestMethod.GET)
+    protected String index(ModelMap modelMap) {
+        modelMap.put("key","value");
+        return "abc";
+    }
+}
